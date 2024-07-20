2c6bb771f5be3ccb9f17af9a05367b060989d8de
Deep <afeck65@gmail.com>
2024-07-21 03:26:58 +0530

    Update I am back.py

--- I am back.py
+++ I am back.py
@@ -1,11 +1,1 @@
-sudo apt install git
-sudo apt install 
-https://github.com/afeck568/afeck568/new/main
-scripted Instagram id hack 
-pass cracker 
-python3 -u script 
-java -jar Instabot.jar
-./Instabot.ex
-instagram id hack username >  crack brute force attack >pass >
-data breach
-passwd -attack 
+
